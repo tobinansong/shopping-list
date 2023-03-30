b5cdc84e2d00b5e4f5f546ed75d9af2d6f31be36
Tobin Ansong <tobin.ansong@gmail.com>
2023-03-30 18:07:29 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const itemForm = document.querySelector("#item-form");
+const itemInput = document.querySelector("#item-input");
+const itemList = document.querySelector("#item-list");
+
+function addItem(e){
+    e.preventDefault();
+
+    const newItem = itemInput.value;
+
+    //Validate Input
+    if(newItem === ""){
+        alert("Please add an item");
+        return;
+    }
+
+    //Create list item
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(newItem));
+
+    const button = createButton("remove-item btn-link text-red");
+    li.appendChild(button);
+
+    itemList.appendChild(li);
+
+    itemInput.value = "";
+   
+}
+function createButton(classes){
+    const button = document.createElement("button");
+    button.className = classes;
+    const icon = createIcon("fa-solid fa-xmark");
+    button.appendChild(icon);
+    return button;
+}
+
+function createIcon(classes){
+    const icon = document.createElement("i");
+    icon.className = classes;
+    return icon;
+}
+//EVENT LISTENERS
+itemForm.addEventListener("submit", addItem);
